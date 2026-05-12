89f5a90070ad6c036f28f5df35d3c342d69e8aa1
abdulrahmansufya <abdulrahman.alsufyani.1@gmail.com>
2026-05-12 20:42:49 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Abdulrahman Al-Sufyani',
+    author_email= 'abdulrahman.alsufyani.1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
